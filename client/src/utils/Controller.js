6fc15a34bcf5102095efcf004c5e002829763c6d
naveen khuntey <96486321+naveen-khuntey@users.noreply.github.com>
2023-06-18 14:29:07 +0530

    initial updated

--- client/src/utils/Controller.js
+++ client/src/utils/Controller.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = process.env.API_URL || "http://localhost:4000";
+const baseUrl = "https://testing-todo-alpha.vercel.app";
 const getallTodo = (setTodos) => {
     axios.get(`${baseUrl}/get`)
     .then(({data}) => {
