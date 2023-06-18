67273867454fe0413c2132af553de8a01ed293ef
naveen khuntey <96486321+naveen-khuntey@users.noreply.github.com>
2023-06-18 13:28:00 +0530

    initial updated

--- client/src/utils/Controller.js
+++ client/src/utils/Controller.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = "http://localhost:4000";
+const baseUrl = process.env.API_URL || "http://localhost:4000";
 const getallTodo = (setTodos) => {
     axios.get(`${baseUrl}/get`)
     .then(({data}) => {
